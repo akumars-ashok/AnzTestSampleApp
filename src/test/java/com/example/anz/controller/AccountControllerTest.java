13c9fd18447f7df15ef97270b2c2e7448eed540e
ashok <ashok_jonnagadla@infosys.com>
2020-01-29 11:22:36 +0530

    Commit_withUnitTestCases

---
+++ src/test/java/com/example/anz/controller/AccountControllerTest.java
@@ -0,0 +1,17 @@
+package com.example.anz.controller;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.example.anz.main.controller.AccountController;
+
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(controllers=AccountController.class)
+public class AccountControllerTest {
+	
+	
+	
+
+}
---
+++ src/test/java/com/example/anz/repository/AccountsRepositoryTest.java
@@ -0,0 +1,18 @@
+package com.example.anz.repository;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+@AutoConfigureTestDatabase(replace=Replace.NONE)
+@ContextConfiguration
+public class AccountsRepositoryTest {
+	
+	
+
+}
